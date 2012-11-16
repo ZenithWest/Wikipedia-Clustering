7b5c52c76468d4eb2f8272a8b5b451bf085f0af0
Brandon West <ZenithWest@aol.com>
2012-11-16 02:11:06 -0600

    Updated Program

--- HNCluster/HNCluster/Form1.Designer.cs
+++ HNCluster/HNCluster/Form1.Designer.cs
@@ -28,80 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(163, 139);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(233, 2);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
             1000000,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(41, 78);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Enabled = false;
-            this.checkBox1.Location = new System.Drawing.Point(180, 248);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(99, 17);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "Site Connected";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Enabled = false;
-            this.checkBox2.Location = new System.Drawing.Point(180, 271);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(118, 17);
-            this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "Page Titles Loaded";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 418);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.ReadOnly = true;
+			this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+			this.numericUpDown1.TabIndex = 0;
+			this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.numericUpDown1.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Enabled = false;
+			this.checkBox1.Location = new System.Drawing.Point(4, 3);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(99, 17);
+			this.checkBox1.TabIndex = 2;
+			this.checkBox1.Text = "Site Connected";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Enabled = false;
+			this.checkBox2.Location = new System.Drawing.Point(109, 3);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(118, 17);
+			this.checkBox2.TabIndex = 3;
+			this.checkBox2.Text = "Page Titles Loaded";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(295, 4);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(76, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Pages Loaded";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Location = new System.Drawing.Point(12, 59);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(738, 594);
+			this.textBox1.TabIndex = 5;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(100, 41);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Previous\r\nPage";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(650, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 41);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Next\r\nPage";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.numericUpDown2);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.checkBox1);
+			this.panel1.Controls.Add(this.numericUpDown1);
+			this.panel1.Controls.Add(this.checkBox2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(191, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(377, 41);
+			this.panel1.TabIndex = 8;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(106, 23);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(33, 13);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Title: ";
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Location = new System.Drawing.Point(44, 21);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.ReadOnly = true;
+			this.numericUpDown2.Size = new System.Drawing.Size(56, 20);
+			this.numericUpDown2.TabIndex = 10;
+			this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.numericUpDown2.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 23);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(38, 13);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "Page: ";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(762, 665);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+		  private System.Windows.Forms.Label label2;
+		  private System.Windows.Forms.Label label3;
+		  private System.Windows.Forms.NumericUpDown numericUpDown2;
     }
 }
 
--- HNCluster/HNCluster/Form1.cs
+++ HNCluster/HNCluster/Form1.cs
@@ -18,66 +18,116 @@ using System.Threading.Tasks;
 
 namespace HNCluster
 {
-    public partial class Form1 : Form
-    {
-        public delegate void IncrementPagesLoadedDelegate();
-        public IncrementPagesLoadedDelegate IncrementPagesLoaded;
+	public partial class Form1 : Form
+	{
+		public delegate void IncrementPagesLoadedDelegate();
+		public IncrementPagesLoadedDelegate IncrementPagesLoaded;
 
-        public delegate void CheckSiteLoadedDelegate();
-        public CheckSiteLoadedDelegate CheckSiteLoaded;
+		public delegate void CheckSiteLoadedDelegate();
+		public CheckSiteLoadedDelegate CheckSiteLoaded;
 
-        public delegate void CheckPageTitlesLoadedDelegate();
-        public CheckPageTitlesLoadedDelegate CheckTitlesLoaded;
+		public delegate void CheckPageTitlesLoadedDelegate();
+		public CheckPageTitlesLoadedDelegate CheckTitlesLoaded;
 
-        public Form1()
-        {
-            InitializeComponent();
-            IncrementPagesLoaded = new IncrementPagesLoadedDelegate(IncrementPagesLoadedMethod);
-            CheckSiteLoaded = new CheckSiteLoadedDelegate(CheckSiteLoadedMethod);
-            CheckTitlesLoaded = new CheckPageTitlesLoadedDelegate(CheckPageTitlesLoadedMethod);
+		public delegate void AddPageTextDelegate(Page page);
+		public AddPageTextDelegate AddPageText;
 
-            /*
-            WikiConnection wiki = new WikiConnection("localhost");
+		bool pressed = false;
+		int currentPage = 0;
+		PageList pageList;
+		Site site;
+
+		public Form1()
+		{
+			InitializeComponent();
+			IncrementPagesLoaded = new IncrementPagesLoadedDelegate(IncrementPagesLoadedMethod);
+			CheckSiteLoaded = new CheckSiteLoadedDelegate(CheckSiteLoadedMethod);
+			CheckTitlesLoaded = new CheckPageTitlesLoadedDelegate(CheckPageTitlesLoadedMethod);
+			AddPageText = new AddPageTextDelegate(AddPageTextMethod);
+			Task.Factory.StartNew(LoadPages);
+			/*
+			WikiConnection wiki = new WikiConnection("localhost");
             
-            GetPage page = new GetPage(wiki, "", "List of trigonometric identities");
-            CommandResult result = page.Execute();*/
+			GetPage page = new GetPage(wiki, "", "List of trigonometric identities");
+			CommandResult result = page.Execute();*/
 
-        }
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Task.Factory.StartNew(LoadPages);
-        }
-
-        void LoadPages()
-        {
-            Site site = new Site("http://localhost/index.php/", "ZenithBot", "Bot");
-            Invoke(CheckSiteLoaded);
-            PageList pageList = new PageList(site);
-            pageList.FillFromAllPages("", 0, false, 2);
-            Invoke(CheckTitlesLoaded);
-            foreach (Page page in pageList)
+		private void button1_Click(object sender, EventArgs e)
+		{/*
+            if (!pressed)
             {
-                page.Load();
-                Invoke(IncrementPagesLoaded);
-                //Application.DoEvents();
-            }
-            int a = 1;
-        }
+                pressed = true;
+                Task.Factory.StartNew(LoadPages);
+            }*/
+		}
 
-        public void IncrementPagesLoadedMethod()
-        {
-            numericUpDown1.Value += 1;
-        }
+		void LoadPages()
+		{
+			site = new Site("http://localhost/index.php/", "ZenithBot", "Bot");
+			Invoke(CheckSiteLoaded);
 
-        public void CheckSiteLoadedMethod()
-        {
-            checkBox1.Checked = true;
-        }
+			Page ComputerSciencePage = new Page(site, "Computer science");
+			ComputerSciencePage.Load();
+			//Invoke(AddPageText, new object[] { ComputerSciencePage.text });
 
-        public void CheckPageTitlesLoadedMethod()
-        {
-            checkBox2.Checked = true;
-        }
-    }
+			pageList = new PageList(site);
+			pageList.FillFromAllPages("", 0, false, 100000);
+			Invoke(CheckTitlesLoaded);
+			foreach (Page page in pageList)
+			{
+				page.Load();
+				if (numericUpDown1.Value == 0)
+				{
+					Invoke(AddPageText, new object[] { pageList[0] });
+				}
+				Invoke(IncrementPagesLoaded);
+				//Application.DoEvents();
+			}
+			int a = 1;
+		}
+
+		public void IncrementPagesLoadedMethod()
+		{
+			numericUpDown1.Value += 1;
+		}
+
+		public void CheckSiteLoadedMethod()
+		{
+			checkBox1.Checked = true;
+		}
+
+		public void CheckPageTitlesLoadedMethod()
+		{
+			checkBox2.Checked = true;
+		}
+
+		public void AddPageTextMethod(Page page)
+		{
+			textBox1.Text = page.text;
+			label2.Text = "Title: " + page.title;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (numericUpDown1.Value > 0 && currentPage > 0)
+			{
+				--currentPage;
+				textBox1.Text = pageList[currentPage].text;
+				label2.Text = "Title: " + pageList[currentPage].title;
+				numericUpDown1.Value = currentPage;
+			}
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			if (numericUpDown1.Value > 0 && currentPage < numericUpDown1.Value - 1)
+			{
+				++currentPage;
+				textBox1.Text = pageList[currentPage].text;
+				label2.Text = "Title: " + pageList[currentPage].title;
+				numericUpDown1.Value = currentPage;
+			}
+		}
+	}
 }
