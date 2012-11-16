6e5fc61c33e4a8d887fee08c68703408522ae0c2
Brandon West <ZenithWest@aol.com>
2012-11-16 01:30:22 -0600

    Added Tools and starting program. It downloads the pages from local repository.

---
+++ HNCluster/HNCluster/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace HNCluster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(163, 139);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(180, 248);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(99, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Site Connected";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(180, 271);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(118, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Page Titles Loaded";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 418);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ HNCluster/HNCluster/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MediaWikiEngine;
+using MediaWikiEngine.Command;
+using MediaWikiEngine.Domain;
+using MediaWikiEngine.Language;
+
+using DotNetWikiBot;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HNCluster
+{
+    public partial class Form1 : Form
+    {
+        public delegate void IncrementPagesLoadedDelegate();
+        public IncrementPagesLoadedDelegate IncrementPagesLoaded;
+
+        public delegate void CheckSiteLoadedDelegate();
+        public CheckSiteLoadedDelegate CheckSiteLoaded;
+
+        public delegate void CheckPageTitlesLoadedDelegate();
+        public CheckPageTitlesLoadedDelegate CheckTitlesLoaded;
+
+        public Form1()
+        {
+            InitializeComponent();
+            IncrementPagesLoaded = new IncrementPagesLoadedDelegate(IncrementPagesLoadedMethod);
+            CheckSiteLoaded = new CheckSiteLoadedDelegate(CheckSiteLoadedMethod);
+            CheckTitlesLoaded = new CheckPageTitlesLoadedDelegate(CheckPageTitlesLoadedMethod);
+
+            /*
+            WikiConnection wiki = new WikiConnection("localhost");
+            
+            GetPage page = new GetPage(wiki, "", "List of trigonometric identities");
+            CommandResult result = page.Execute();*/
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(LoadPages);
+        }
+
+        void LoadPages()
+        {
+            Site site = new Site("http://localhost/index.php/", "ZenithBot", "Bot");
+            Invoke(CheckSiteLoaded);
+            PageList pageList = new PageList(site);
+            pageList.FillFromAllPages("", 0, false, 2);
+            Invoke(CheckTitlesLoaded);
+            foreach (Page page in pageList)
+            {
+                page.Load();
+                Invoke(IncrementPagesLoaded);
+                //Application.DoEvents();
+            }
+            int a = 1;
+        }
+
+        public void IncrementPagesLoadedMethod()
+        {
+            numericUpDown1.Value += 1;
+        }
+
+        public void CheckSiteLoadedMethod()
+        {
+            checkBox1.Checked = true;
+        }
+
+        public void CheckPageTitlesLoadedMethod()
+        {
+            checkBox2.Checked = true;
+        }
+    }
+}
