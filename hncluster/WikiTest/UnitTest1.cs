3962644eef4042142a16a0e0cdc1e3620d3ff552
Brice Scott <bscott0214@gmail.com>
2012-11-20 11:47:57 -0600

    Addition of WikiTest project for unit testing of Wiki classes

---
+++ hncluster/WikiTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WikiTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
